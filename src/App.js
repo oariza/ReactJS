4ea2861337fdf3a28cd2769b9195a5db5402afde
oariza <arizaomar@gmail.com>
2020-05-17 22:01:09 -0500

    Clock component

--- src/App.js
+++ src/App.js
@@ -2,88 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-
-// -- FUNCTIONAL COMPONENT --
-//function App() {
-//  return (
-//    <div className="App">
-//      <header className="App-header">
-//        <img src={logo} className="App-logo" alt="logo" />
-//        <p>
-//          Edit <code>src/App.js</code> and save to reload.
-//        </p>
-//        <a
-//          className="App-link"
-//          href="https://reactjs.org"
-//          target="_blank"
-//          rel="noopener noreferrer"
-//        >
-//          Learn React
-//        </a>
-//      </header>
-//    </div>
-//  );
-//}
-
 // Components
 import Clock from './components/Clock/Clock'
-import Card from './components/Card/Card'
-import Counter from './components/Counter/Counter'
-import Button from './components/Button'
-import Notes from './components/Notes'
 
-const data = [
-  {
-    'category':'PROGRAMMING',
-    'title':'8 VS Code Extensions You Might Love',
-    'description':'CodeSnap, GitHub Markdown Preview, Paste JSON as Code, and more',
-    'author':'Marvin Wendt in Better Programming',
-    'publicationDate':'May 4',
-    'readTime':'3 min read',
-  },
-  {
-    'category':'PROGRAMMING',
-    'title':'Title 2',
-    'description':'CodeSnap, GitHub Markdown Preview, Paste JSON as Code, and more',
-    'author':'Marvin Wendt in Better Programming',
-    'publicationDate':'May 10',
-    'readTime':'5 min read',
-  },
-  {
-    'category':'PROGRAMMING',
-    'title':'Title 3',
-    'description':'CodeSnap, GitHub Markdown Preview, Paste JSON as Code, and more',
-    'author':'Marvin Wendt in Better Programming',
-    'publicationDate':'May 10',
-    'readTime':'5 min read',
-  }
-]
+
 
 export default class App extends Component {
-
-  _renderCards() {
-    return data.map(({
-      category,
-      title,
-      description,
-      author,
-      publicationDate,
-      readTime
-    }) => {
-      return (
-        <Card 
-          category={category}
-          title={title}
-          description={description}
-          author={author}
-          publicationDate={publicationDate}
-          readTime={readTime}
-        />
-      )
-    })
-  }
-
   render () {
     return (
       <div className="App">
@@ -93,14 +17,7 @@ export default class App extends Component {
             Bootcamp white - <strong>React</strong>
           </h1>
           <Clock />
-          <Counter />
-          <Button />
-          <Notes />
-        </header>
-        <div className='cards-container'>
-          {this._renderCards()}
-        </div>
-        
+        </header>     
       </div>
     )
   }
