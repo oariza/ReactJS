719ebe9c53a723b4558d280ed60152e21577f022
oariza <arizaomar@gmail.com>
2020-05-21 13:33:49 -0500

    Credit Card Component

--- src/pages/CreditCard/index.js
+++ src/pages/CreditCard/index.js
@@ -8,16 +8,32 @@ export default class Notes extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      brand: 'Visa',
-      creditCardNumber: 1234567891234567,
-      userName: 'OMAR ARIZA',
-      expMonth: '12',
-      expYear: '23'
+      creditCardNumber: '',
+      userName: '',
+      expMonth: 0,
+      expYear: 0
     };
+    this.handleInput = this.handleInput.bind(this)
+  }
+
+  handleInput({target: {name, value}}) {
+    this.setState({
+      [name]: value
+    })
+    
   }
 
   render() {
-    let { brand, creditCardNumber, userName, expMonth, expYear} = this.state
+    const { creditCardNumber, userName, expMonth, expYear} = this.state
+    let numberSlice = creditCardNumber.slice(0,2)
+    let brand
+    if (numberSlice >= 40 && numberSlice <= 49){
+      brand = "Visa"
+    }
+    if (numberSlice >=51 && numberSlice <= 55){
+      brand = "Mastercard"
+    }
+
     return (
       <div className="Container">
         <div>
@@ -28,10 +44,38 @@ export default class Notes extends Component {
             }
           />
           <div className="CreditCard-container">
-            <div className= 'Brand-container' >{brand}</div>
-            <div className= 'Number-container' >{creditCardNumber}</div>
-            <div className= 'user-container' >{userName}</div>
-            <div className= 'Date-container' >{expMonth}/{expYear}</div>            
+            <p className= 'Brand-container' >{brand}</p>
+            <p className= 'Number-container' >{creditCardNumber}</p>
+            <p className= 'user-container' >{userName}</p>
+            <p className= 'Date-container' >{expMonth}/{expYear}</p>            
+          </div>
+          <div>
+            <form>            
+              <input
+                value={creditCardNumber}
+                onChange={this.handleInput}
+                placeholder={"Credit Card number"}
+                name={"creditCardNumber"}
+              />
+              <input
+                value={userName}
+                onChange={this.handleInput}
+                placeholder={"Name"}
+                name={"userName"}
+              />
+              <input
+                value={expMonth}
+                onChange={this.handleInput}
+                placeholder={"Expiration Month"}
+                name={"expMonth"}
+              />
+              <input
+                value={expYear}
+                onChange={this.handleInput}
+                placeholder={"Expiration Year"}
+                name={"expYear"}
+              />
+            </form>
           </div>
         </div>
       </div>
